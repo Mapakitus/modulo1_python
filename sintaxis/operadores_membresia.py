a66111876e8a26ba71c5adc43c1df117f6329cb1
Mapakitus <mapakitus@gmail.com>
2025-10-07 09:57:59 +0200

    Agregar script para demostrar el uso de operadores de membresía

---
+++ sintaxis/operadores_membresia.py
@@ -0,0 +1,17 @@
+print("\n")
+print("=" * 20)
+print("OPERADORES DE MEMBRESÍA")
+print("=" * 20)
+print("\n")
+
+nombre = 'Pepe'
+nombre_completo = 'Pepe García'
+
+print(nombre in nombre_completo)  # True
+print(nombre not in nombre_completo)  # False   
+
+nombre = 'Paco'
+nombre_completo = 'Pepe García'
+
+print(nombre in nombre_completo)  # True
+print(nombre not in nombre_completo)  # False 
