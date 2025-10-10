b302b1401c461c0a48c39bf43d237c392e49f2ea
Mapakitus <mapakitus@gmail.com>
2025-10-10 13:43:51 +0200

    Agregar comentario para aclarar el uso de .strip() en la entrada de email

--- sintaxis/funciones_login_intentos_limitados.py
+++ sintaxis/funciones_login_intentos_limitados.py
@@ -13,12 +13,13 @@ def login():
     # bucle for para máximo 3 intentos fallidos de verificar email y contraseña
     # leer por input el email y password       
     for intento in range(1, MAX_INTENTOS + 1): # 1, 2, 3
-        email = input("Introduce email: ").strip()
+        email = input("Introduce email: ").strip() # .strip() elimina espacios al inicio y final
         password = input("Introduce password: ").strip()
         
         if email == VALID_EMAIL and password == VALID_PASSWORD:
             return True
         
+        # si no coinciden, informar de los intentos restantes
         intentos_restantes = MAX_INTENTOS - intento # 3 - 1 = 2, 3 - 2 = 1, 3 - 3 = 0
         if intentos_restantes > 0:
             print(f"Credenciales incorrectas, te quedan {intentos_restantes} intentos")
