e3e727b6489350558049bda446dad9441b9902e8
Mapakitus <mapakitus@gmail.com>
2025-10-17 10:27:15 +0200

    import date ejemplos

---
+++ sintaxis/import_fechas.py
@@ -0,0 +1,13 @@
+import datetime 
+
+#date para fechas sin tiempo
+fecha_actual = datetime.date.today()
+print(f"Fecha actual: {fecha_actual}")
+
+# datetime para fecha con tiempo:
+ahora = datetime.datetime.now()
+print(f"Hora actual: {ahora}")
+
+#time solo tiempo:
+tiempo = datetime.time(10, 30)
+print(f"Hora actual: {tiempo}")
